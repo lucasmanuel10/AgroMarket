1617cce233d5c563e498c33c4f38ed8c778ac65a
jaca-pereira <62773204+jaca-pereira@users.noreply.github.com>
2020-11-28 01:04:09 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -3,15 +3,16 @@ var current_user = null;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyBSnUkR2G22khdwPxWkJ3AdY1PmmuiKFpw",
-    authDomain: "fir-js-67501.firebaseapp.com",
-    databaseURL: "https://fir-js-67501.firebaseio.com",
-    projectId: "fir-js-67501",
-    storageBucket: "fir-js-67501.appspot.com",
-    messagingSenderId: "367010020114",
-    appId: "1:367010020114:web:b21f92d0652acdb17111cb",
-    measurementId: "G-30WDYQMQLB"
+    apiKey: "AIzaSyA2JNM8baGJWfSa59WQd3-8P-9zqC9TV3U",
+    authDomain: "projeto1-cbb1a.firebaseapp.com",
+    databaseURL: "https://projeto1-cbb1a.firebaseio.com",
+    projectId: "projeto1-cbb1a",
+    storageBucket: "projeto1-cbb1a.appspot.com",
+    messagingSenderId: "366062903132",
+    appId: "1:366062903132:web:f319507ffa693e32fd2b99",
+    measurementId: "G-T5155MKZ4N"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -51,10 +52,12 @@ function register(first_name, last_name, email, password, account_type) {
 
 
 function get_products() {
-    var products = [];
-    for (var product in app.products) {
-        products.push(product);
-    }
+    var products = null;
+    firebase.database().ref('products/').on('value', function(snapshot) { 
+        products = snapshot.val; 
+    });
+    if (products == null)
+        return [];
     return products;
 }
 
