f96883a50f2c1059006b7a15d4fa6fcf039f96e6
jaca-pereira <62773204+jaca-pereira@users.noreply.github.com>
2020-11-28 03:18:30 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -22,7 +22,7 @@ firebase.analytics();
 function login(email, password) {
     var user = null;
     firebase.database().ref('users/' + email.split("@")[0]).once('value').then(function(snapshot){
-        user = snapshot.val();    
+        return user = snapshot.val();    
     }).then(function(){
         if(user == null || user.Password!=password)
             return false;
