b2911fec17ecb0f559af88c47aab2e74e7021142
jaca-pereira <62773204+jaca-pereira@users.noreply.github.com>
2020-11-28 22:55:35 +0000

    Update backend.js

--- backend.js
+++ backend.js
@@ -90,11 +90,13 @@ function get_product(product_name, callback) {
 function get_products(callback) {
     firebase.database().ref('product/').once('value').then(function(snapshot){
         var products = snapshot.val();
+        console.log(products);
         if(products == null) {
             products_ = []; 
             callback(products_);  
         } else {
            var products_ = [];
+          
            snapshot.forEach(function(childSnapshot) {
                 products_.push(childSnapshot.toJSON());
            }); 
